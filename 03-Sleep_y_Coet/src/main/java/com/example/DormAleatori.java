a236069245089e87490fe71a4c84b1b231243d3e
LoniousSosa <lsosa19@jaumebalmes.net>
2025-01-13 15:24:46 +0100

    Parte 1 practica 3

--- 03-Sleep_y_Coet/src/main/java/com/example/DormAleatori.java
+++ 03-Sleep_y_Coet/src/main/java/com/example/DormAleatori.java
@@ -1,5 +1,35 @@
 package com.example;
 
-public class DormAleatori {
-    
+import java.util.Random;
+
+public class DormAleatori extends Thread {
+
+    public DormAleatori(String name){
+        super(name);
+        
+    }
+    public static void main(String[] args) {
+        DormAleatori joan = new DormAleatori("Joan");
+        DormAleatori pep = new DormAleatori("Pep");
+
+        joan.start();
+        pep.start();
+    }
+
+    @Override
+    public void run() {
+        int sleepTime= 0;
+        for (int i = 0; i < 10; i++) {
+            Random random = new Random();
+            int randomNumber = random.nextInt(1000) + 1;
+            sleepTime = sleepTime + randomNumber; 
+            System.out.printf("%s(%d) a dormir %dms total %d%n", this.getName(), i, randomNumber, sleepTime);
+            try {
+                sleep(randomNumber);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    };
 }
